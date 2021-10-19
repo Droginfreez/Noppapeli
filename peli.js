f5de59a0ddb512092461f8de6c06cc976d196ffb
Joonatan Karhu <joonatan.kar@gmail.com>
2021-10-19 12:13:28 +0300

    projekti edistyy

--- peli.js
+++ peli.js
@@ -1,19 +1,50 @@
+var tempScore = 0;
+var round = 0;
+var turn = 1;
+var pisteet = 0;
 var nimet = [];
 var i;
-for (i = 0; i < 2; i++) {
-    nimet.push ((prompt("Syötä pelaajien nimet" , "")));
+var o = 1;
+var maara = [];
+var result= 0;
+
+
+
+ maara.push ((prompt("Syötä pelaajien määrä numerona", "")));
+
+for (i = 0; i < maara; i++) {
+    nimet.push ((prompt("Syötä pelaajan " + o++ + " nimi" , "")));
+}
+for (let i = 0; i < nimet.length; i++) {
+  document.writeln(`${nimet[i]}` + " pisteet: " + pisteet +"<br>");
 }
 
-for (let i = 0; i < nimet.length; i++) {
-    document.writeln(`${i+1}. ${nimet[i]}` + "<br>");
-}
 
 
 
 
 function rollDice(){
+  var result = 0;
+  var content = document.createTextNode("Round: " + round + " Turn: "+ turn + " Points: " + result);
+
     var die1 = document.getElementById("die1");
     var status = document.getElementById("status");
     var d1 = Math.floor(Math.random() * 6) + 1;
     die1.innerHTML = d1;
+
+
+ if (d1==1){
+  document.getElementById("status").innerHTML = "Sait 1. Vuoro vaihtuu";
+    result=result +0;
+    tempScore=0;
+    round=round +1;
+    addScore(result)
+   
+} else {
+  document.getElementById("status").innerHTML = "Sait "+d1+".";
+    result=result +d1;
+    tempScore=tempScore+result;
+    round=round +1;
+
+}
 }
